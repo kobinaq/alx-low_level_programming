7a751e39d519798e75e75da214dbcfebc34fcb11
kobinaq <kobinastudios@gmail.com>
2023-05-29 12:40:56 +0000

    task 5 comoplete

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	while (x <= 9)
+	{
+		printf("%d", x);
+		x++;
+	}
+	putchar ('\n');
+	return (0);
+}
