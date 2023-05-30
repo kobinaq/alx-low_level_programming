dee68b89deb2124a24384225195595f0db4e9c57
kobinaq <kobinastudios@gmail.com>
2023-05-30 11:11:23 +0000

    Task 6 and 7 complete

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int x = '0';
+
+	while (x <= '9')
+	{
+		putchar(x);
+		x++;
+	}
+		putchar ('\n');
+	return (0);
+}
