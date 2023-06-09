c4cb5929f9f0cc61be43ee21b77d1bc3fc957fba
kobinaq <kobinastudios@gmail.com>
2023-06-09 21:40:43 +0000

    Task 0 Update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0
  */
-int main()
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -15,6 +15,6 @@ int main()
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	
+
 	return (0);
 }
