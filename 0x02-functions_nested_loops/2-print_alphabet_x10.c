c477b6e18a6691538437d5326764caf0e6f69992
kobinaq <kobinastudios@gmail.com>
2023-06-29 02:31:45 +0000

    Task 2 done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - It prints the letters of the alphabeth.
+ *
+ *Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char l;
+	int i;
+
+	for (i = '0'; i <= 9; i++)
+	{
+		for (l = 'a'; l <= 'z'; l++)
+		{
+			_putchar(l);
+		}
+	}
+	_putchar('\n');
+}
