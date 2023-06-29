4924c29eba5ca226cf01eb3d2b0cf6198372b2b2
kobinaq <kobinastudios@gmail.com>
2023-06-29 15:07:16 +0000

    Task 7 cpmplete

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @i: input value for function print_last_digit
+ * Return: The value of the last digit
+ */
+
+int print_last_digit(int i)
+{
+	if (i < 0 || i >= 0)
+	{
+		i = i % 10;
+
+		_putchar (i);
+		return (i);
+	}
+	else 
+		return (1);
+}
