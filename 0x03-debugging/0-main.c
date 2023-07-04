c4d734e72c0fa79d8c0a76acd5b7d8dbc3aab5c8
kobinaq <kobinastudios@gmail.com>
2023-07-04 13:01:25 +0000

    Task 0 complete

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* @i: is an intger
+* Return: 0
+*
+*/
+
+void positive_or_negative(int i);
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
