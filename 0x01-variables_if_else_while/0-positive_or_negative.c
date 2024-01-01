96b9b3be3d0b7471098273bc197e6fae0e13f849
kobinaq <kobinastudios@gmail.com>
2024-01-01 18:55:35 +0000

    Task 0 update

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 /**
  *main - entry point
@@ -8,7 +9,10 @@
 
 int main(void)
 {
-	int n = 0;
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 		printf("%d is positive\n", n);
