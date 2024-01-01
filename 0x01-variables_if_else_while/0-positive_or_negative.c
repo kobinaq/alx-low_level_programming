6a3eaa7f9437b6a8c483f50a50d89b64c235d59f
kobinaq <kobinastudios@gmail.com>
2024-01-01 18:31:45 +0000

    Task 0 update

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,17 +2,19 @@
 
 /**
  *main - entry point
- *@n: integer to be tested
- *
+ *n - integer to be tested
+ *Return: 0 means success
  */
 
-int main()
+int main(void)
 {
 	int n = 0;
 
-	if (n == 0)
-		printf ("%d is zero\n", n);
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
 
 	else if (n < 0)
-		printf ("%d is negative\n", n);
+		printf("%d is negative\n", n);
 }
