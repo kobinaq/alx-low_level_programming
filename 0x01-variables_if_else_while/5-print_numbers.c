3cb4a0f34be8a051e9d1da0a97220627bb6c22e4
kobinaq <kobinastudios@gmail.com>
2024-01-03 13:36:59 +0000

    Task 5 complete

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: 0 means success
+ */
+
+int main(void)
+{
+	for (int a = '0'; a <= '9'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
