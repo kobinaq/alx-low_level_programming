d184948c2aacc0ea379a4f0b04b474f557db85e6
kobinaq <kobinastudios@gmail.com>
2024-01-05 20:08:05 +0000

    Header file and Task 1 upload

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_alphabet -  prints the alphabet, in lowercase, followed by a new line.
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+}
