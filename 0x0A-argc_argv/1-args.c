f235c93699bff53ccb32ed41dbbe64b453776650
kobinaq <kobinastudios@gmail.com>
2024-01-26 15:24:51 +0000

    Task 1 completed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - Entry Point
+ *@argc: counts the number of command line arguments
+ *@argv: array of command line arguments
+ *Return: Always 0
+ */
+
+int main(int argc, int **argv)
+{
+	printf("%d\n", argc);
+
+	return(0);
+}
