e305a45e83495725d31595f4fc4efe0f3c9772e0
kobinaq <kobinastudios@gmail.com>
2024-07-06 17:08:49 +0000

    task 11 completed

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0
+ */
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int count = 0;
+
+	while (a < 8)
+	{
+		b = a + 1;
+		while (b < 9)
+		{
+			c = b + 1;
+			while (c < 10)
+			{
+				if (count > 0)
+					putchar (',');
+					putchar(' ');
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(c + '0');
+				count++;
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
