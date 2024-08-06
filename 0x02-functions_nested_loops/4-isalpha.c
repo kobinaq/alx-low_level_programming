aa844c786c62f362998b94574406f93c49866ab0
kobinaq <kobinastudios@gmail.com>
2024-08-06 17:19:46 +0000

    task 4 updated

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
 
 /**
  *_isalpha -  checks for alphabetic character
- *@c: variable to be checked for alphabetic character.
+ *@c: variable to be checked for alphabetic character
  *Return: 1 if c is a letter, lowercase or uppercase
  */
 
