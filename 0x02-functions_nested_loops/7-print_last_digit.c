985970fd2c263e3ec341aa42c12844aa98dc1dfa
kobinaq <kobinastudios@gmail.com>
2024-08-06 19:40:21 +0000

    Task 7 update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,10 +9,5 @@
 int print_last_digit(int n)
 {
 	int last_digit = n % 10;
-
-	/* If the last digit is negative, make it positive */
-	if (last_digit < 0)
-		last_digit = -last_digit;
-
 	return (last_digit);
 }
