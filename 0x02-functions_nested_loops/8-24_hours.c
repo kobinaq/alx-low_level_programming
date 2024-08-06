0b03b10cf9ff07c0ac5fce7335b698c96d6b3c26
kobinaq <kobinastudios@gmail.com>
2024-08-06 19:38:57 +0000

    Task 8 compalted

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day from 00:00 to 23:59.
+ *
+ * Description: This function prints each minute of a 24-hour day,
+ * starting from 00:00 to 23:59, using the format HH:MM.
+ */
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
+
