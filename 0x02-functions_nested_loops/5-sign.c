9dc6cfab96aa29bccec6d33beba9619669e41a62
kobinaq <kobinastudios@gmail.com>
2024-08-06 17:30:14 +0000

    Task 5 completed

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: input number for printsign function
+ * Return: 1 if n > 0 and 0 of n < 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	if (n < 0)
+		return (0);
+}
