33712185ec52f55fde1db583701ba8ca7d8d18dc
kobinaq <kobinastudios@gmail.com>
2024-08-06 17:13:28 +0000

    TAsk 12 updated

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,41 +1,42 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
- *Return: Always 0
+ * put_two_digits - Prints a number with two digits
+ * main - Entry point
+ * Return: Always 0
+ * @n: input digit
  */
-
-#include <stdio.h>
 
 void put_two_digits(int n)
 {
-    putchar(n / 10 + '0');
-    putchar(n % 10 + '0');
+	putchar(n / 10 + '0');
+	putchar(n % 10 + '0');
 }
 
 int main(void)
 {
-    int i = 0;
-    int j;
+	int i = 0;
+	int j;
 
-    while (i <= 99)
-    {
-        j = i + 1;
-        while (j <= 99)
-        {
-            put_two_digits(i);
-            putchar(' ');
-            put_two_digits(j);
-            if (i != 98 || j != 99)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-            j++;
-        }
-        i++;
-    }
-    putchar('\n');
-    return (0);
+	while (i <= 99)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			put_two_digits(i);
+			putchar(' ');
+			put_two_digits(j);
+
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
 
