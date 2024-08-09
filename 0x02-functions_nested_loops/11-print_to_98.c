88cb207b85eefd8c117dd85f82a17ca6242eb538
kobinaq <kobinastudios@gmail.com>
2024-08-09 15:35:47 +0000

    Task 11 completed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,19 +1,44 @@
 #include "main.h"
 
 /**
+ * print_number - prints numbers
  * print_to_98 - prints numbers in progression up to 98
  * @n: input number
  */
+
+void print_number(int n);
 
 void print_to_98(int n)
 {
 	while (n != 98)
 	{
-		_putchar(n + '0');
+		print_number(n);
+		_putchar(',');
+		_putchar(' ');
 		if (n < 98)
 			n++;
 		else
 			n--;
 	}
-	_putchar(n + '0');
+	print_number(98);
+	_putchar('\n');
+}
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n >= 10)
+	{
+		if (n >= 100)
+		{
+			_putchar((n / 100) + '0');
+			n %= 100;
+		}
+		_putchar((n / 10) + '0');
+	}
+	_putchar((n % 10) + '0');
 }
