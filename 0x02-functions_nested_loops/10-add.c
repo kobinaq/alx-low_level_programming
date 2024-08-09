4440afe67aa02a1dbf0c2f12e823818ebd887cf1
kobinaq <kobinastudios@gmail.com>
2024-08-09 14:14:09 +0000

    Task 20 completed

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two digits together
+ * @a: first number
+ * @b: second number
+ * Return: sum of the two numbers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
