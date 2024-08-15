0db95f97d9f8b609a546bf5cde5cad2408f1fce0
kobinaq <kobinastudios@gmail.com>
2024-08-15 14:22:34 +0000

    Task 4 and 5 update

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,8 +2,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints the numbers, from 0 to 9, followed by new line
- * Return: Always 0
+ * print_most_numbers - prints the numbers, from 0 to 9, followed by new lin
  */
 
 void print_most_numbers(void)
@@ -14,6 +13,7 @@ void print_most_numbers(void)
 	{
 		if (k == 2 || k == 4)
 		{
+			k++;
 			continue;
 		}
 		_putchar(k + '0');
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,22 +3,23 @@
 
 /**
  * more_numbers - prints 10x the numbers, from 0 to 14, followed by a new line
- * Return: Always 0
  */
 
 void more_numbers(void)
 {
-	int k = 0;
+	int k;
 	int b = 0;
 
 	while (b < 10)
 	{
+		k = 0;
 		while (k < 15)
 		{
-			_putchar(k + '0');
+			_putchar((k / 10) + '0');
+			_putchar((k % 10) + '0');
 			k++;
 		}
+		_puctchar('\n');
 		b++;
 	}
-	_putchar('\n');
 }
