aa0bd72984c87ac7ed4fa1dee5813fed767ee066
kobinaq <kobinastudios@gmail.com>
2024-08-15 14:01:31 +0000

    Task 3 completed

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+	int k = 0;
+
+	while (k < 10)
+	{
+		_putchar(k + '0');
+		k++;
+	}
+}
