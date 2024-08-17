2cc0a8dc48475102162163216dde09e312a61d77
kobinaq <kobinastudios@gmail.com>
2024-08-17 17:56:42 +0000

    Task 9 update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,56 +1,66 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - Entry point
+ *
+ *
+ */
+
+int _putchar(char c)
+{
+    return putchar(c);
+}
+
 void print_number(int n)
 {
-    if (n >= 10)
-        print_number(n / 10);
-    _putchar((n % 10) + '0');
+	if (n >= 10)
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
 }
 
 int main(void)
 {
-    int i = 1;
+	int i = 1;
 
-    while (i <= 100)
-    {    
-    	if (i % 3 == 0 && i % 5 == 0)
-        {
-            _putchar('F');
-            _putchar('i');
-            _putchar('z');
-            _putchar('z');
-            _putchar('B');
-            _putchar('u');
-            _putchar('z');
-            _putchar('z');
-        }
-        else if (i % 3 == 0)
-        {
-            _putchar('F');
-            _putchar('i');
-            _putchar('z');
-            _putchar('z');
-        }
-        else if (i % 5 == 0)
-        {
-            _putchar('B');
-            _putchar('u');
-            _putchar('z');
-            _putchar('z');
-        }
-        else
-        {
-            print_number(i);
-        }
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+		}
+		else if (i % 3 == 0)
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+		}
+		else if (i % 5 == 0)
+		{
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+		}
+		else
+		{
+			print_number(i);
+		}
 
-        if (i < 100)
-        {
-            _putchar(' ');
-        }
-        i++;
-    }
-    _putchar('\n');
-
-    return (0);
+		if (i < 100)
+		{
+			_putchar(' ');
+		}
+		i++;
+	}
+	_putchar('\n');
+	return (0);
 }
