e49f5ff91db2ceae21ba66d159b46c9f5676e976
kobinaq <kobinastudios@gmail.com>
2024-08-17 14:58:14 +0000

    task 6 and 7 completed

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: input number for the nunber of lines
+ *
+ */
+
+void print_line(int n)
+{
+	int a = 0;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		while (a <= n)
+		{
+			_putchar ('_');
+			a++;
+		}
+		_putchar ('\n');
+	}
+}
