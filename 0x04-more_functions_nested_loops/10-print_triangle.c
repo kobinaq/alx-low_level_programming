ca4f3db9b26543c0598894a02b4ef7937ade0864
kobinaq <kobinastudios@gmail.com>
2024-08-19 10:50:42 +0000

    Task 10 update

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,35 +6,35 @@
  */
 void print_triangle(int size)
 {
-    int i = 1;
-    int j;
+	int i = 1;
+	int j;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        while (i <= size)
-        {
-            j = 1;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (i <= size)
+		{
+			j = 1;
 
-            while (j <= size)
-            {
-                if (j <= size - i)
-                {
-                    _putchar(' ');
-                }
-                else
-                {
-                    _putchar('#');
-                }
-                j++;
-            }
-            _putchar('\n');
-            i++;
-        }
-    }
+			while (j <= size)
+			{
+				if (j <= size - i)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+				j++;
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
 }
 
 
